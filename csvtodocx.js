d9dfaf50eb4f58543f3d1840a647e4980084272d
bmacmurray99 <b.macmur@gmail.com>
2020-02-08 21:46:01 -0500

    Updated to accept CSVs with whitespace and dashes

--- csvtodocx.js
+++ csvtodocx.js
@@ -5,19 +5,35 @@ const yargs = require("yargs").argv;
 var inputFile = yargs.input;
 var outputFile = yargs.output;
 var templateFile =yargs.template;
-var jObj;
+var jsonObj;
 
-var jsonData = csv({
-		
-					})
+var regex = /\s|\-|\%|\&|/g
+  
+
+var jsonData = csv()
 	.fromFile(inputFile)
 	.then((jsonObj)=>{
-		console.log(jsonObj);
-		jObj = jsonObj;
-		return jObj;
+		//console.log(jsonObj.length);
+		console.log(Object.keys(jsonObj[0]))
+		preparedHeadJson = [];
+		for (i=0;i<jsonObj.length; i++){
+			preparedHeadJson.push({});
+			keys = Object.keys(jsonObj[i]);
+			for (j = 0; j< keys.length; j++){
+			preparedHeadJson[i][keys[j].replace(regex,"_")] = jsonObj[i][keys[j]];
+
+			}
+			
+		};
+		console.log(Object.keys(preparedHeadJson[0]))
+		return preparedHeadJson
 	});
+
  
+
 
  docTemplates({template:templateFile,output:outputFile,
 	data:{jsonData}},
-	function(err){console.log(err)});
+	(err)=>{console.log(err)});
+
+
